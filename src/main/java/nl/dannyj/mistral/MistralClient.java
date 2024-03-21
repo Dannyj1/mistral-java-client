74fc3d9baac897338785ecb447805382ab9fdb47
Danny Jelsma <73849717+Dannyj1@users.noreply.github.com>
2024-03-21 13:40:37 +0100

    Don't error when Mistral changes their API and adds a new field (DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES set to false)

--- src/main/java/nl/dannyj/mistral/MistralClient.java
+++ src/main/java/nl/dannyj/mistral/MistralClient.java
@@ -17,6 +17,7 @@
 package nl.dannyj.mistral;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.ConstraintViolationException;
 import lombok.Getter;
@@ -230,6 +231,7 @@ public class MistralClient {
         ObjectMapper mapper = new ObjectMapper();
 
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
     }
 }
