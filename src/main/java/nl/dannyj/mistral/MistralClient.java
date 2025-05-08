f15b5e715591ab298ce4c6f55108462a0a4f5088
Danny Jelsma <73849717+Dannyj1@users.noreply.github.com>
2025-05-08 16:31:16 +0200

    refactor: Remove unnecessary dependency cycle between MistralClient and HttpService

--- src/main/java/nl/dannyj/mistral/MistralClient.java
+++ src/main/java/nl/dannyj/mistral/MistralClient.java
@@ -251,7 +251,7 @@ public class MistralClient {
      * @return A new instance of MistralService
      */
     private MistralService buildMistralService() {
-        return new MistralService(this, new HttpService(this));
+        return new MistralService(this, new HttpService(this.httpClient));
     }
 
     /**
